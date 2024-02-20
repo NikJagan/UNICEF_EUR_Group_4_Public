1e2085259d4467834dffc5f03779bed64bf5c87b
marleenhaubitz <131541048+marleenhaubitz@users.noreply.github.com>
2024-02-21 00:32:10 +0100

    new

---
+++ shapley.R
@@ -0,0 +1,28 @@
+load("/Users/marleenhaubitz/Downloads/RandomForest3")
+predictions <- as.data.frame(predict(rf_model, df_test))
+colnames(predictions)[1] <- "online_donation_count"
+
+X <- df_train[, !(names(df_train)) %in% c("online_donation_count")]
+X_test <- df_test[-which(names(df_test) == "online_donation_count")] 
+response <- as.vector(df_train$online_donation_count)
+
+#Create a "Predictor" object that holds model and data
+predictor_all <- Predictor$new(rf_model, data = X, y = response)
+
+#Display and plot results
+shapley_all <- Shapley$new(predictor_all, x.interest = X_test[3260, ])
+shapley_all
+shapley_all$plot()
+
+
+#PLOT THE DISTRIBUTION 
+ggplot(data = predictions, aes(x = online_donation_count)) + 
+	geom_histogram(binwidth = 0.5, fill = "skyblue", color = "black") +
+	labs(x = "Online Donations Predictions")
+
+ggplot(data = df_train, aes(x = online_donation_count)) + 
+	geom_histogram(binwidth = 0.5, fill = "skyblue", color = "black") +
+	labs(x = "Online Donations Train")
+
+
+
