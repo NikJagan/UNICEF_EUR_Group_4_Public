b569e8c719abf9a3c0af17656d9d3c73fe8f543c
ganastas <anastasgeraskina@gmail.com>
2024-02-25 11:30:22 +0100

    Create correct file structure in git

--- Posthoc analysis.r
+++ Posthoc analysis.r
@@ -10,16 +10,18 @@ rf_model <- tuned_model
 #define the prediction func
 yhat <- function(X.model, newdata) as.numeric(predict(X.model, newdata))
 
-names(df_train)
-str(df_train)
+#define the colour palette
+colour_palette <- c("#caf0f8","#ade8f4", "#90e0ef", "#48cae4", "#00b4d8", "#0096c7", "#0077b6", "#023e8a")
+
 
 #create ALE 1st order plots for most important vars
 #pledges
-ALE_pledge <- ALEPlot(df_train, rf_model, pred.fun=yhat, J="pledge_ind",K=20, NA.plot = F) 
-plot(ALE_pledge$x.values, ALE_pledge$f.values, type="l", xlab="Presence of a pledge", ylab="Effect on DV", main="ALE main effect of pledges on online donations")
+ALE_pledge <- ALEPlot(df_train, rf_model, pred.fun=yhat, J="pledge_ind",K=20, NA.plot = F)
+ALE_pledge
+barplot(ALE_pledge$x.values, ALE_pledge$f.values, type="h", xlab="Presence of a pledge", ylab="Effect on DV", main="ALE main effect of pledges on online donations")
 dev.copy(png,'pledges.png')
 dev.off()
-
+help(plot)
 #demographics
 ned <- ALEPlot(df_train, rf_model, pred.fun=yhat, J="Nederlandse.achtergrond",K=20, NA.plot = T)
 afrika <- ALEPlot(df_train, rf_model, pred.fun=yhat, J="Afrika",K=20, NA.plot = F)
@@ -60,20 +62,33 @@ dev.off()
 
 #create ALE 2nd order plots for proximities
 #culture
+#plot 3 biggest nationality groups
+nl <- ALEPlot(df_train, rf_model, pred.fun=yhat, J=c("Nederland_ind", "Nederlandse.achtergrond"),K=20, NA.plot = F)
+vorm_nl <- ALEPlot(df_train, rf_model, pred.fun=yhat, J=c("Nederland_ind", "X.voormalige..Nederlandse.Antillen..Aruba"),K=20, NA.plot = F)
+EU <- ALEPlot(df_train, rf_model, pred.fun=yhat, J=c("Europe_ind", "Europa..excl..Nederlandse.achtergrond."),K=20, NA.plot = F)
+
+#plot 3 nationalities historically connected with NL
+ind <- ALEPlot(df_train, rf_model, pred.fun=yhat, J=c("Indonesie_ind", "Indonesië"),K=20, NA.plot = F)
+surinam <- ALEPlot(df_train, rf_model, pred.fun=yhat, J=c("Suriname_ind", "Suriname"),K=20, NA.plot = F)
+turkey <- ALEPlot(df_train, rf_model, pred.fun=yhat, J=c("Turkije_ind", "Turkije"),K=20, NA.plot = F)
+
+#plot 3 nationalities with most external events
+africa <- ALEPlot(df_train, rf_model, pred.fun=yhat, J=c("Afrika_ind", "Afrika"),K=20, NA.plot = F)
+america <- ALEPlot(df_train, rf_model, pred.fun=yhat, J=c("Amerika_ind", "Amerika"),K=20, NA.plot = F)
+asia <- ALEPlot(df_train, rf_model, pred.fun=yhat, J=c("Asie_ind", "Azië"),K=20, NA.plot = F)
+
 png("culture_interaction.png", width = 6, height = 10, units = 'in', res = 500)
 par(mfrow = c(3,3), mar=c(4, 4, 3, 1), oma=c(3,3,3,1))
-ALEPlot(df_train, rf_model, pred.fun=yhat, J=c("Nederland_ind", "Nederlandse.achtergrond"),K=20, NA.plot = F)
-ALEPlot(df_train, rf_model, pred.fun=yhat, J=c("Nederland_ind", "X.voormalige..Nederlandse.Antillen..Aruba"),K=20, NA.plot = F)
-ALEPlot(df_train, rf_model, pred.fun=yhat, J=c("Turkije_ind", "Turkije"),K=20, NA.plot = F)
+image(as.integer(nl$x.values[[1]]), nl$x.values[[2]], nl$f.values, xlab = "Event happened in the Netherlands", ylab = "% of Dutch people in zipcode", col=colour_palette)
+image(as.integer(vorm_nl$x.values[[1]]), vorm_nl$x.values[[2]], vorm_nl$f.values, xlab = "Event happened in the Netherlands Antilles", ylab = "% of Dutch people in zipcode", col=colour_palette)
+image(as.integer(EU$x.values[[1]]), EU$x.values[[2]], EU$f.values, xlab = "Event happened in Europe (excl.NL)", ylab = "% of European people in zipcode", col=colour_palette)
 mtext("ALE effect plot for cultural proximities", side=3, line=1.5, cex=0.7, outer=TRUE)
-mtext("% of this nationality in the zip code", side=2, line=1.5, cex=0.7, outer=TRUE)
-mtext("Region where an event happened (1=happened)", side=1, line=1.5, cex=0.7, outer=TRUE)
-ALEPlot(df_train, rf_model, pred.fun=yhat, J=c("Indonesie_ind", "Indonesië"),K=20, NA.plot = F)
-ALEPlot(df_train, rf_model, pred.fun=yhat, J=c("Suriname_ind", "Suriname"),K=20, NA.plot = F)
-ALEPlot(df_train, rf_model, pred.fun=yhat, J=c("Afrika_ind", "Afrika"),K=20, NA.plot = F)
-ALEPlot(df_train, rf_model, pred.fun=yhat, J=c("Amerika_ind", "Amerika"),K=20, NA.plot = F)
-ALEPlot(df_train, rf_model, pred.fun=yhat, J=c("Asie_ind", "Azië"),K=20, NA.plot = F)
-ALEPlot(df_train, rf_model, pred.fun=yhat, J=c("Oceanie_ind", "Oceanië"),K=20, NA.plot = F)
+image(as.integer(ind$x.values[[1]]), ind$x.values[[2]], ind$f.values, xlab = "Event happened in Indonesia", ylab = "% of Indonesian people in zipcode", col=colour_palette)
+image(as.integer(surinam$x.values[[1]]), surinam$x.values[[2]], surinam$f.values, xlab = "Event happened in Surinam", ylab = "% of Surinamese people in zipcode", col=colour_palette)
+image(as.integer(turkey$x.values[[1]]), turkey$x.values[[2]], turkey$f.values, xlab = "Event happened in Turkey", ylab = "% of Turkish people in zipcode", col=colour_palette)
+image(as.integer(africa$x.values[[1]]), africa$x.values[[2]], africa$f.values, xlab = "Event happened in Africa", ylab = "% of African people in zipcode", col=colour_palette)
+image(as.integer(america$x.values[[1]]), america$x.values[[2]], america$f.values, xlab = "Event happened in America", ylab = "% of American people in zipcode", col=colour_palette)
+image(as.integer(asia$x.values[[1]]), asia$x.values[[2]], asia$f.values, xlab = "Event happened in Asia", ylab = "% of Asian people in zipcode", col=colour_palette)
 dev.off()
 
 #location
