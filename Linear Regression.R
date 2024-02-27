cefc7b988d94426eff5c5466ef0adb1a0e78f32c
ganastas <anastasgeraskina@gmail.com>
2024-02-27 15:08:33 +0100

    Creates readable plots for LR

--- Linear Regression.R
+++ Linear Regression.R
@@ -1,12 +1,14 @@
 # Step 1: import the train and test datasets
-data_train <- read.csv("full_data_train.csv")
-data_test <- read.csv("full_data_test.csv")
+data_train <- read.csv("datasets/full_data_train.csv")
+data_test <- read.csv("datasets/full_data_test.csv")
 
 
 # Step 2: transformations before performing Linear Regression 
 # - transform columns Nederland_ind:pledge_ind into categorical variables 
+#Remove columns without observations
 data_train[5:19] <- lapply(data_train[5:19], as.factor)
 data_test[5:19] <- lapply(data_test[5:19], as.factor)
+data_train <- data_train[, !(names(data_train) %in% c("Marokko_ind", "Belgie_ind", "Duitsland_ind", "Polen_ind"))]
 
 
 # Step 3: perform the linear regression 
@@ -17,9 +19,9 @@ data_test[5:19] <- lapply(data_test[5:19], as.factor)
 lr_online <- lm(online_donation_count ~ . - total_donations_count - Postcode - Year - Week + 
                   Nederland_ind:Nederlandse.achtergrond + Europe_ind:Nederlandse.achtergrond + 
                   World_ind:Nederlandse.achtergrond + 
-                  Europe_ind:Europa..excl..Nederlandse.achtergrond. + Marokko_ind:Marokko + 
-                  Turkije_ind:Turkije + Belgie_ind:België + Duitsland_ind:Duitsland + 
-                  Indonesie_ind:Indonesië + Polen_ind:Polen + Suriname_ind:Suriname + 
+                  Europe_ind:Europa..excl..Nederlandse.achtergrond. + 
+                  Turkije_ind:Turkije + 
+                  Indonesie_ind:Indonesië +  Suriname_ind:Suriname + 
                   Oceanie_ind:Oceanië + Afrika_ind:Afrika + Amerika_ind:Amerika + 
                   Asie_ind:Azië + Nederland_ind:X.voormalige..Nederlandse.Antillen..Aruba + 
                   pledge_ind:Nederlandse.achtergrond + pledge_ind:Afrika + 
@@ -84,31 +86,42 @@ coefficients <- (coef(lr_online)[-1])  # Exclude the intercept term
 
 
 # 2. Sort coefficients in descending order and get the order
+#Only take top 3 and bottom 3 to present
 order_indices <- order(coefficients)
 sorted_coefficients <- coefficients[order_indices]
 sorted_feature_names <- names(coefficients)[order_indices]
+ind_end <- length(sorted_coefficients)
+sorted_6coefficients <- sorted_coefficients[c(ind_end, ind_end-1,ind_end-2,3,2,1)]
+sorted_6feature_names <- sorted_feature_names[c(ind_end, ind_end-1,ind_end-2,3,2,1)]
 
 # 3. Create a horizontal bar plot
-barh <- barplot(sorted_coefficients, names.arg = sorted_feature_names, col = "#1CABE2",
+unicef_blue <- "#00aeef"
+png("fig14_LR_feat_imp.png", width = 10, height = 8, units = 'in', res = 300)
+par(mar=c(5,10,3,1))
+barplot(sorted_6coefficients, col = unicef_blue,
                 main = "Feature Importance in Linear Regression",
-                xlab = "Coefficient Values", ylab = "Features", horiz = TRUE, las = 1, cex.names = 0.5)
-
+                xlab = "Coefficient Values", ylab = "", horiz = TRUE, las = 1, cex.lab=1.5, cex.main=2,
+                names.arg=c("Proximity for Oceania", "Indonesian bg", "Pledge for German bg", "Polish bg", "Proximity for Dutch", "Oceania bg"))
+dev.off()
 
 
 # Construct the marginal effect plots using the sjPlot package
 library(sjPlot)
 
 # marginal effect plots for the pledges * migration backgrounds interaction terms
-plot_model(lr_online, type = "pred", terms = c("pledge_ind", "Nederlandse.achtergrond"))
-plot_model(lr_online, type = "pred", terms = c("pledge_ind", "Afrika"))
-plot_model(lr_online, type = "pred", terms = c("pledge_ind", "Amerika"))
-plot_model(lr_online, type = "pred", terms = c("pledge_ind", "Azië"))
-plot_model(lr_online, type = "pred", terms = c("pledge_ind", "Europa..excl..Nederlandse.achtergrond."))
-plot_model(lr_online, type = "pred", terms = c("pledge_ind", "Oceanië"))
-plot_model(lr_online, type = "pred", terms = c("pledge_ind", "Indonesië"))
-plot_model(lr_online, type = "pred", terms = c("pledge_ind", "X.voormalige..Nederlandse.Antillen..Aruba"))
-plot_model(lr_online, type = "pred", terms = c("pledge_ind", "Suriname"))
-plot_model(lr_online, type = "pred", terms = c("pledge_ind", "Turkije"))
+par(mfrow=c(5,2), mar=c(5,10,3,1), oma=c(2,2,2,2))
+mtext("Marginal effect plots for the pledges * migration backgrounds interaction terms", side=3, outer=TRUE)
+p1<-plot_model(lr_online, type = "pred", terms = c("pledge_ind", "Nederlandse.achtergrond"))
+p2<-plot_model(lr_online, type = "pred", terms = c("pledge_ind", "Afrika"))
+p3<-plot_model(lr_online, type = "pred", terms = c("pledge_ind", "Amerika"))
+p4<-plot_model(lr_online, type = "pred", terms = c("pledge_ind", "Azië"))
+p5<-plot_model(lr_online, type = "pred", terms = c("pledge_ind", "Europa..excl..Nederlandse.achtergrond."))
+p6<-plot_model(lr_online, type = "pred", terms = c("pledge_ind", "Oceanië"))
+p7<-plot_model(lr_online, type = "pred", terms = c("pledge_ind", "Indonesië"))
+p8<-plot_model(lr_online, type = "pred", terms = c("pledge_ind", "X.voormalige..Nederlandse.Antillen..Aruba"))
+p9<-plot_model(lr_online, type = "pred", terms = c("pledge_ind", "Suriname"))
+p10<-plot_model(lr_online, type = "pred", terms = c("pledge_ind", "Turkije"))
+g<- grid.arrange(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10, nrow=5, top = "Marginal effect plots for the pledges * migration backgrounds interaction terms")
 
 # marginal effect plots for the local proximity captured by the Dutch background * local vs European vs. rest of the world events
 plot_model(lr_online, type = "pred", terms = c("Nederland_ind", "Nederlandse.achtergrond"))
