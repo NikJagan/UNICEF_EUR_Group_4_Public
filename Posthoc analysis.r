608e5fab4e74a8d712434349dfb9ef90dd109323
ganastas <anastasgeraskina@gmail.com>
2024-02-27 14:21:40 +0100

    Removes shapley into a separate file

--- Posthoc analysis.r
+++ Posthoc analysis.r
@@ -239,31 +239,3 @@ axis(1, at=c(0,1),labels=c("0","1"), cex.axis=1.2)
 mtext("Presence of pledge in zip code (1=pledge happened)", side=1, line=1, cex=1.5, outer=TRUE)
 mtext("% of specific cultural background in zipcode", side=2, line=1, cex=1.5, outer=TRUE)
 dev.off()
-
-################################
-#########SHAPLEY################
-################################
-predictions <- as.data.frame(predict(tuned_model, df_test))
-colnames(predictions)[1] <- "online_donation_count"
-
-X <- df_train[, !(names(df_train)) %in% c("online_donation_count")]
-X_test <- df_test[-which(names(df_test) == "online_donation_count")] 
-response <- as.vector(df_train$online_donation_count)
-
-#Create a "Predictor" object that holds model and data
-predictor_all <- Predictor$new(tuned_model, data = X, y = response)
-
-#Display and plot results
-shapley_all <- Shapley$new(predictor_all, x.interest = X_test[2, ])
-shapley_all
-shapley_all$plot()
-
-
-#PLOT THE DISTRIBUTION 
-ggplot(data = predictions, aes(x = online_donation_count)) + 
-	geom_histogram(binwidth = 0.5, fill = "skyblue", color = "black") +
-	labs(x = "Online Donations Predictions")
-
-ggplot(data = df_train, aes(x = online_donation_count)) + 
-	geom_histogram(binwidth = 0.5, fill = "skyblue", color = "black") +
-	labs(x = "Online Donations Train")
